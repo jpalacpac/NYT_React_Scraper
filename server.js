5ed332bc40e8fba62be9896b92e07b5de2e5d5ea
Jonathan Wong <jdwong2@yahoo.com>
2017-08-14 14:01:05 -0700

    Initial server file

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Article = require("./models/Article");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/group-nyt-react");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/api/saved", function(req, res) {
+
+});
+
+app.post("/api/saved", function(req, res) {
+
+});
+
+app.delete("/api/saved", function(req, res) {
+
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
